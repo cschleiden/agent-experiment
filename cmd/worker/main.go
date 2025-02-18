6480ce8101587abfd76c35b1b85ac0f7804e3006
Christopher Schleiden <cschleiden@live.de>
2025-02-18 10:15:22 -0800

    Load .env file into process

--- cmd/worker/main.go
+++ cmd/worker/main.go
@@ -3,13 +3,20 @@ package main
 import (
 	"context"
 	"fmt"
+	"log"
 
 	"github.com/cschleiden/agent-experiment"
 	"github.com/cschleiden/go-workflows/backend/sqlite"
 	"github.com/cschleiden/go-workflows/worker"
+	"github.com/joho/godotenv"
 )
 
 func realMain() error {
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("Error loading .env file:", err)
+	}
+
 	b := sqlite.NewInMemoryBackend()
 	worker := worker.New(b, &worker.DefaultOptions)
 
